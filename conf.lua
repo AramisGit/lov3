4627a4dcd91f50be04f3ae1561d363c165c9abe3
AramisGit <55937372+AramisGit@users.noreply.github.com>
2026-07-16 17:52:43 +1000

    Add files via upload
    added conf

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.title = "lov3"
+    t.window.resizable = true
+    t.window.width = 600
+    t.window.height = 600
+    t.window.depth = 24
+    t.window.vsync = 0
+    t.console = false
+end
